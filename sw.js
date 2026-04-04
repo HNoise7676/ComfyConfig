d9217fa95e62d4228cc4ff94731527f4a04619cb
Violet/HNoise 💜 <251638193+HNoise7676@users.noreply.github.com>
2026-04-04 12:56:31 +0200

    another background change that will hopefully fix stuff

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  // Must exist for the "Install" button to show up
-  event.respondWith(fetch(event.request));
+    event.respondWith(fetch(event.request));
 });
