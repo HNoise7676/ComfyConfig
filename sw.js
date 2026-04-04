cbca21c9c105427873bda3f808da002b9453e16f
Violet/HNoise 💜 <251638193+HNoise7676@users.noreply.github.com>
2026-04-04 12:54:44 +0200

    some background changes

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
 self.addEventListener('fetch', (event) => {
+  // Must exist for the "Install" button to show up
   event.respondWith(fetch(event.request));
 });
